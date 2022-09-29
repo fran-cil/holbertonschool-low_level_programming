5eacd867137ff66ebe1d258acbfe44a240f22ecf
fran-cil <franciscoclintano18@gmail.com>
2022-09-28 19:17:52 -0700

    Task 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase
+ *
+ * Return: 0
+ */
+
+int main (void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+
+	putchar(10);
+
+	return (0);
+}
