b9490e3fb218e603c3333a0d20463b0368f271fa
fran-cil <franciscoclintano18@gmail.com>
2022-10-01 07:53:29 -0700

    Task 0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char test = 'a';
+
+	_putchar(test);
+
+	return (0);
+}
---
+++ functions_nested_loops/main.h
@@ -0,0 +1,30 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*Libraries*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+
+/*Prototypes*/
+
+int _putchar(char c);
+
+void print_alphabet(void);
+
+void print_alphabet_x10(void);
+
+int _islower(int c);
+
+int _isalpha(int c);
+
+int print_sign(int n);
+
+int _abs(int);
+
+int print_last_digit(int);
+
+void jack_bauer(void);
+
+#endif
