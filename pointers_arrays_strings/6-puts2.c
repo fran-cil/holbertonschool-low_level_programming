7ec5e426cb3efc8a406a9315e5f3af3da18101b8
fran-cil <franciscoclintano18@gmail.com>
2022-10-08 15:16:02 -0700

    Task 6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other character of a string
+ * @str: String
+ */
+
+void puts2(char *str)
+{
+	int len;
+	int i;
+
+	for (len = 0; str[len] != '\0'; len++)
+	{
+	}
+
+	for (i = 0; i < len; i+=2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar(10);
+}
