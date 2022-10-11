4d134594e8c6bf420db9202dcdbfeac9bb2253c7
fran-cil <franciscoclintano18@gmail.com>
2022-10-11 14:28:39 -0700

    Task 5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *string_toupper - changes all lowercase letters of a string to uppercase
+ * @lowtoupp: String to be changed
+ * Return: Changed string
+ */
+
+char *string_toupper(char *lowtoupp)
+{
+	int i;
+
+	for (i = 0; lowtoupp[i] != '\0'; i++)
+	{
+		if (lowtoupp[i] >= 'a' && lowtoupp[i] <= 'z')
+		{
+			lowtoupp[i] = lowtoupp[i] -32;
+		}
+	}
+	return (lowtoupp);
+}
