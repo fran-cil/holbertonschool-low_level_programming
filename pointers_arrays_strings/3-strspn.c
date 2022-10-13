1fb2aabd2671071a1986a3892091898ca95a882a
fran-cil <franciscoclintano18@gmail.com>
2022-10-13 14:13:20 -0700

    Task 3

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strspn - Gets the length of a prefix substring
+ * @accept: Number of bytes
+ * @s: String
+ * Return: i
+ **/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i;
+	unsigned int j;	
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+				break;
+		}
+		if (!accept[j])
+			break;
+	}
+	return (i);
+}
