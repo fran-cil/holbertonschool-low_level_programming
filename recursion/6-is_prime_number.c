2b82219fbf2700205dcc7d47ac4cbef089db7a80
fran-cil <franciscoclintano18@gmail.com>
2022-10-18 17:43:10 -0700

    Task 6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,35 +1,42 @@
 #include "main.h"
 /**
- * is_prime_number - Checks if a number is prime or not
- * @n: input number
- * Return: 0
+ * is_prime_number - Return 1 if n is prime, otherwise 0
+ * @n: Number
+ * Return: 1 or 0
  */
 
 int is_prime_number(int n)
 {
-	if (n <= 1)
-	{
-		return (0);
-	}
-	return (is_prime_number2(n, 2));
+	return (is_prime_number2(n - 1, n));
 }
 
 /**
- * is_prime_number2 - Defines the return value
- * @num: Input number
- * @i: Counter
- * Return: 0
- */
+ * is_prime_number2 -Return value if prime or not
+ * @i: Base
+ * @n:Number
+ * Return: 0 not prime, 1 prime
+ **/
 
-int is_prime_number2(int num, int i)
+int is_prime_number2(int i, int n)
 {
-	if (num == i)
-	{
-		return (1);
-	}
-	if (num % i == 0)
+	if (n < 0)
 	{
 		return (0);
 	}
-	return (is_prime_number2(num, i + 1));
+	else if (n == 1)
+	{
+		return (0);
+	}
+
+	else if (n % i != 0)
+	{
+		return (is_prime_number2(i - 1, n));
+	}
+
+	else if (n % i == 0 && i > 1)
+	{
+		return (0);
+	}
+	else
+		return (1);
 }
