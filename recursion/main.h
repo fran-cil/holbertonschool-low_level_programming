4dc527ca29e1ed7af47631a4de40f1da1c038269
fran-cil <franciscoclintano18@gmail.com>
2022-10-18 18:35:28 -0700

    Main

--- recursion/main.h
+++ recursion/main.h
@@ -15,6 +15,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
+int is_prime_number2(int num, int i);
 int _putchar(char c);
 
 #endif
