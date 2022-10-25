64036db964f01c191834a0a6a4ebe9b077e87f76
fran-cil <franciscoclintano18@gmail.com>
2022-10-24 17:18:38 -0700

    Task 2

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * *str_concat - Returns a pointer to a newly allocated space in memory
+ * which contains a copy of the string given as a parameter
+ * @s1: String 1.
+ * @s2: String 2.
+ * Return: Concatenation of s1 and s2
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *cat;
+	int l;
+	int j;
+	int i;
+	int k;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	for (l = 0; s1[l] != '\0'; l++)
+		;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+	cat = malloc(((l + j) + 1) * sizeof(char));
+
+	if (cat == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		cat[i] = s1[i];
+	}
+	for (k = 0; s2[k] != '\0'; k++)
+	{
+		cat[i] = s2[k];
+		i++;
+	}
+	return (cat);
+}
